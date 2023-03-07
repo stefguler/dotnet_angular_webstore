ce8011cb4e953d4675307fc7b7e8556bdb852a6e
stef <stef.guler@gmail.com>
2023-03-07 18:24:35 +0100

    first init

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    //public IActionResult Index()
+    //{
+    //    return View();
+    //}
+
+    [HttpGet]
+    public string GetProduct()
+    {
+        return "this will be a list of products!";
+    }
+
+
+    [HttpGet("{id}")]
+    public string GetProduct(int id)
+    {
+        return $"this will return a product with id: {id}!";
+    }
+
+}
